08ad1081981f1ddc5e603060b025344e95defe75
2met7cen <101178618+2met7cen@users.noreply.github.com>
2026-01-15 14:22:11 +0300

    Update anim_loop.js

--- anim_loop.js
+++ anim_loop.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
             renderer: 'svg',
             loop: true,
             autoplay: true,
-            path: el.dataset.lottie
+            path: el.dataset.lottieLoop
         });
     });
 });
